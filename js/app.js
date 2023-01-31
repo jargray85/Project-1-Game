0a1cae68679f0e455f5de826de0e42b22b7adf81
JamesRJGray <jamesrjgray@gmail.com>
2023-01-30 21:22:13 -0600

    added more resources, styles, and a few functions

--- js/app.js
+++ js/app.js
@@ -2,3 +2,32 @@ console.log("Working...")
 
 // define variable for transition items
 let background = document.querySelector("background-image")
+let mainDialogue = document.querySelector("title")
+let itemButtons = document.querySelector("buttons")
+let choiceButtons = document.querySelector("choice-buttons")
+
+// hide buttons functions
+function hideItemButtons() {
+    itemButtons.style.display = 'none';
+}
+
+function hideChoiceButtons() {
+    choiceButtons.style.display = 'none';
+}
+
+// function to transition to next background image 
+function inventoryTransition() {
+    background.src = "project1_assets/inventory-screen.jpg"
+}
+
+function monsterOneTransition() {
+    background.src = "project1_assets/monster1-background.jpg"
+}
+
+function monsterTwoTransition() {
+    background.src = "project1_assets/monster2-background.jpg"
+}
+
+function monsterThreeTransition() {
+    background.src = "project1_assets/monster3-background.jpg"
+}
